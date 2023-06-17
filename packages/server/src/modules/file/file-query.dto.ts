dda3d1ef2d289b1ceee6605f916a034b82bbb07d
Nepsyn <nepsyn@outlook.com>
2023-06-17 23:54:05 +0800

    feat: Add api in `FileController`

---
+++ packages/server/src/modules/file/file-query.dto.ts
@@ -0,0 +1,46 @@
+import { ApiQueryDto } from '../../utils/api.query.dto';
+import { File } from './file.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsInt, IsOptional, IsString } from 'class-validator';
+
+export class FileQueryDto extends ApiQueryDto<File> {
+  @ApiProperty({
+    description: '查询关键字',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  keyword?: string;
+
+  @ApiProperty({
+    description: '文件id',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  id?: string;
+
+  @ApiProperty({
+    description: '文件md5',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  md5?: string;
+
+  @ApiProperty({
+    description: '文件是否过期',
+    required: false,
+  })
+  @IsOptional()
+  @IsBoolean()
+  expired?: boolean;
+
+  @ApiProperty({
+    description: '上传用户id',
+    required: false,
+  })
+  @IsOptional()
+  @IsInt()
+  userId?: number;
+}

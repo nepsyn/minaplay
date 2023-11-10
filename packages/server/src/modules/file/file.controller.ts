0158fc28e7b28502392902f3008ce6229e2b1183
Nepsyn <nepsyn@outlook.com>
2023-11-10 22:06:45 +0800

    refactor: Remove filename encoding conversion in file uploading

--- packages/server/src/modules/file/file.controller.ts
+++ packages/server/src/modules/file/file.controller.ts
@@ -66,7 +66,6 @@ export class FileController {
         fileSize: 10485760, // 10MB
       },
       fileFilter(req, file, callback) {
-        file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
         if (VALID_IMAGE_MIME.includes(file.mimetype)) {
           callback(null, true);
         } else {
@@ -118,7 +117,6 @@ export class FileController {
         fileSize: 4294967296, // 10GB
       },
       fileFilter(req, file, callback) {
-        file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
         if (VALID_VIDEO_MIME.includes(file.mimetype)) {
           callback(null, true);
         } else {

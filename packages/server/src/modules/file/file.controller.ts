49fd1a4e824ad45f3594465c28d02932c50b8be0
Nepsyn <nepsyn@outlook.com>
2023-12-01 13:05:21 +0800

    fix: Replace `StreamFile` with `sendFile` & `download` API

--- packages/server/src/modules/file/file.controller.ts
+++ packages/server/src/modules/file/file.controller.ts
@@ -10,7 +10,6 @@ import {
   Post,
   Query,
   Res,
-  StreamableFile,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -178,37 +177,26 @@ export class FileController {
   @ApiOperation({
     description: '原始文件数据',
   })
-  async getRawFileById(@Param('id') id: string, @Res({ passthrough: true }) res: Response) {
+  async getRawFileById(@Param('id') id: string, @Res() res: Response) {
     const file = await this.fileService.findOneBy({ id });
     if (!file || !file.isExist) {
       throw buildException(NotFoundException, ErrorCodeEnum.NOT_FOUND);
     }
 
-    if (file.mimetype) {
-      res.setHeader('Content-Type', file.mimetype);
-    }
-    return new StreamableFile(fs.createReadStream(file.path));
+    res.sendFile(file.path);
   }
 
   @Get([':id/download', ':id/download/:name'])
   @ApiOperation({
     description: '下载文件',
   })
-  async downloadFileById(
-    @Param('id') id: string,
-    @Res({ passthrough: true }) res: Response,
-    @Param('name') name?: string,
-  ) {
+  async downloadFileById(@Param('id') id: string, @Res() res: Response, @Param('name') name?: string) {
     const file = await this.fileService.findOneBy({ id });
     if (!file || !file.isExist) {
       throw buildException(NotFoundException, ErrorCodeEnum.NOT_FOUND);
     }
 
-    if (file.mimetype) {
-      res.setHeader('Content-Type', file.mimetype);
-    }
-    res.setHeader('Content-Disposition', `attachment; filename="${name ?? file.name}"`);
-    return new StreamableFile(fs.createReadStream(file.path));
+    res.download(file.path, name ?? file.name);
   }
 
   @Get()

da5da3198c70e4d3b24b6c3078a57b2e8fb51110
Nepsyn <nepsyn@outlook.com>
2024-03-14 14:52:46 +0800

    feat: Update get latest updated episodes API

--- packages/server/src/modules/media/episode/episode.controller.ts
+++ packages/server/src/modules/media/episode/episode.controller.ts
@@ -14,9 +14,8 @@ import {
 import { EpisodeService } from './episode.service.js';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RequirePermissions } from '../../authorization/require-permissions.decorator.js';
-import { PermissionEnum } from '../../../enums/permission.enum.js';
+import { ErrorCodeEnum, PermissionEnum } from '../../../enums/index.js';
 import { buildException } from '../../../utils/build-exception.util.js';
-import { ErrorCodeEnum } from '../../../enums/error-code.enum.js';
 import { EpisodeDto } from './episode.dto.js';
 import { AuthorizationGuard } from '../../authorization/authorization.guard.js';
 import { SeriesService } from '../series/series.service.js';
@@ -24,19 +23,15 @@ import { Episode } from './episode.entity.js';
 import { buildQueryOptions } from '../../../utils/build-query-options.util.js';
 import { Between } from 'typeorm';
 import { EpisodeQueryDto } from './episode-query.dto.js';
-import { ViewHistoryService } from '../view-history/view-history.service.js';
 import { ApiPaginationResultDto } from '../../../common/api.pagination.result.dto.js';
+import { ApiQueryDto } from '../../../common/api.query.dto.js';
 
 @Controller('series/episode')
 @UseGuards(AuthorizationGuard)
 @ApiTags('series')
 @ApiBearerAuth()
 export class EpisodeController {
-  constructor(
-    private seriesService: SeriesService,
-    private viewHistoryService: ViewHistoryService,
-    private episodeService: EpisodeService,
-  ) {}
+  constructor(private seriesService: SeriesService, private episodeService: EpisodeService) {}
 
   @Get()
   @ApiOperation({
@@ -57,6 +52,20 @@ export class EpisodeController {
       skip: query.page * query.size,
       take: query.size,
       order: { [query.sort]: query.order },
+    });
+
+    return new ApiPaginationResultDto(result, total, query.page, query.size);
+  }
+
+  @Get('update')
+  @ApiOperation({
+    description: '查询更新单集',
+  })
+  @RequirePermissions(PermissionEnum.ROOT_OP, PermissionEnum.MEDIA_OP, PermissionEnum.MEDIA_VIEW)
+  async queryUpdateEpisodes(@Query() query: ApiQueryDto<Episode>) {
+    const [result, total] = await this.episodeService.findUpdateAndCount({
+      skip: query.page * query.size,
+      take: query.size,
     });
 
     return new ApiPaginationResultDto(result, total, query.page, query.size);
--- packages/server/src/modules/media/episode/episode.service.ts
+++ packages/server/src/modules/media/episode/episode.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Episode } from './episode.entity.js';
-import { DeepPartial, FindManyOptions, FindOneOptions, FindOptionsWhere, Repository } from 'typeorm';
+import { DataSource, DeepPartial, FindManyOptions, FindOneOptions, FindOptionsWhere, In, Repository } from 'typeorm';
 
 @Injectable()
 export class EpisodeService {
-  constructor(@InjectRepository(Episode) private episodeRepository: Repository<Episode>) {}
+  constructor(
+    @InjectRepository(Episode) private episodeRepository: Repository<Episode>,
+    private dataSource: DataSource,
+  ) {}
 
   async save(episode: DeepPartial<Episode>) {
     return await this.episodeRepository.save(episode);
@@ -21,6 +24,42 @@ export class EpisodeService {
 
   async findAndCount(options?: FindManyOptions<Episode>) {
     return await this.episodeRepository.findAndCount(options);
+  }
+
+  async findUpdateAndCount(options?: FindManyOptions<Episode>) {
+    const resultRaw: { id: number }[] = await this.dataSource
+      .createQueryBuilder()
+      .select('id,updateAt,pubAt')
+      .from(
+        (qb) =>
+          qb
+            .select('id,seriesId,updateAt,pubAt')
+            .addSelect('ROW_NUMBER() OVER (PARTITION BY seriesId ORDER BY pubAt DESC,updateAt DESC)', 'row_num')
+            .from(Episode, 'ep'),
+        'ranked',
+      )
+      .where({ row_num: 1 })
+      .orderBy('pubAt', 'DESC')
+      .addOrderBy('updateAt', 'DESC')
+      .skip(options.skip)
+      .take(options.take)
+      .getRawMany();
+    const result = await this.episodeRepository.find({
+      where: {
+        id: In(resultRaw.map(({ id }) => id)),
+      },
+      order: {
+        pubAt: 'DESC',
+        updateAt: 'DESC',
+      },
+    });
+    const countRaw: { total: number }[] = await this.episodeRepository
+      .createQueryBuilder()
+      .select('COUNT(DISTINCT seriesId)', 'total')
+      .getRawMany();
+    const count = countRaw[0].total;
+
+    return [result, count] as const;
   }
 
   async delete(where: FindOptionsWhere<Episode>) {

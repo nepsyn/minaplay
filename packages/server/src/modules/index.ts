b03cb8d7fbb81e58cdc8363a6d28e499d9ffee25
Nepsyn <nepsyn@outlook.com>
2025-01-05 20:37:13 +0800

    feat: Export modules in @minaplay/server

--- packages/server/src/modules/index.ts
+++ packages/server/src/modules/index.ts
@@ -1,9 +1,11 @@
 export * from './authorization/permission.entity.js';
 export * from './authorization/action-log.entity.js';
 export * from './authorization/authorization.service.js';
+export * from './authorization/authorization.module.js';
 
 export * from './file/file.entity.js';
 export * from './file/file.service.js';
+export * from './file/file.module.js';
 
 export * from './live/live.entity.js';
 export * from './live/live-chat.entity.js';
@@ -12,6 +14,7 @@ export * from './live/live.service.js';
 export * from './live/live-chat.service.js';
 export * from './live/live-voice.service.js';
 export * from './live/live-stream.service.js';
+export * from './live/live.module.js';
 
 export * from './media/media.entity.js';
 export * from './media/series/series.entity.js';
@@ -25,11 +28,13 @@ export * from './media/series/series-tag.service.js';
 export * from './media/series/series-subscribe.service.js';
 export * from './media/episode/episode.service.js';
 export * from './media/view-history/view-history.service.js';
+export * from './media/media.module.js';
 
 export * from './notification/notification-meta.entity.js';
 export * from './notification/notification.service.js';
 export * from './notification/notification-meta.service.js';
 export * from './notification/notification-service-adapter.interface.js';
+export * from './notification/notification.module.js';
 
 export * from './plugin/plugin.service.js';
 export * from './plugin/plugin.interface.js';
@@ -38,6 +43,7 @@ export * from './plugin/plugin-ref.js';
 export * from './plugin/plugin-listener-context.js';
 export * from './plugin/plugin.decorator.js';
 export * from './plugin/constants.js';
+export * from './plugin/plugin.module.js';
 
 export * from './subscribe/rule/rule.entity.js';
 export * from './subscribe/rule/rule-error-log.entity.js';
@@ -52,8 +58,11 @@ export * from './subscribe/parse-log/parse-log.service.js';
 export * from './subscribe/rule/rule.interface.js';
 export * from './subscribe/download/download-item-state.interface.js';
 export * from './subscribe/download/download-task.interface.js';
+export * from './subscribe/subscribe.module.js';
 
 export * from './system/system.service.js';
+export * from './system/system.module.js';
 
 export * from './user/user.entity.js';
 export * from './user/user.service.js';
+export * from './user/user.module.js';
